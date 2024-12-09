2953e70cf9fb10eb9922255d2464baf7b3c92e7e
Connoye <95093333+Connoye@users.noreply.github.com>
2024-12-08 20:53:07 -0500

    Project initialization
    Take home assignment project initialization for CompuZign

---
+++ src/main/java/artifactory_saas/compuzign_home_assignment/CompuzignHomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package artifactory_saas.compuzign_home_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompuzignHomeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompuzignHomeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/artifactory_saas/compuzign_home_assignment/CompuzignHomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package artifactory_saas.compuzign_home_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompuzignHomeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
